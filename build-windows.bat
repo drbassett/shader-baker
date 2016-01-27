ccfc0cffc46f10c4d060f5a364a56d7d18b0c191
drbassett <dale.r.bassett@gmail.com>
2016-01-26 22:02:11 -0500

    We have a window!

--- build-windows.bat
+++ build-windows.bat
@@ -11,8 +11,10 @@ set releaseOptions=/MT /O2 /Oi
 
 set ignoredWarnings=/wd4100
 
+set libraries=User32.lib
+
 mkdir %outputDir% 2> nul
 
 pushd %outputDir% > nul
-cl.exe /nologo /W4 /WX %ignoredWarnings% %debugOptions% /Gm- %srcDir%\win32.cpp /Fd%projectName% /Fe%projectName% /INCREMENTAL:NO
+cl.exe /nologo /W4 /WX %ignoredWarnings% %debugOptions% /Gm- %srcDir%\win32.cpp /Fd%projectName% /Fe%projectName% /INCREMENTAL:NO %libraries%
 popd
--- src/win32.cpp
+++ src/win32.cpp
@@ -4,11 +4,64 @@
 
 #pragma warning(pop)
 
+#include <stdio.h>
+
+LRESULT CALLBACK windowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine,
 	int nCmdShow)
 {
+	const char windowClassName[] = "Shader Baker Class";
+
+	WNDCLASS wc = {};
+	wc.lpfnWndProc = windowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = windowClassName;
+
+	RegisterClassA(&wc);
+
+	HWND window = CreateWindowExA(
+		0,
+		windowClassName,
+		"Shader Baker",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	if (!window)
+	{
+		printf("Failed to create window");
+		return 1;
+	}
+
+	ShowWindow(window, nCmdShow);
+
+	MSG message = {};
+	while (GetMessageA(&message, NULL, 0, 0))
+	{
+		TranslateMessage(&message);
+		DispatchMessageA(&message);
+	}
+
 	return 0;
+}
+
+LRESULT CALLBACK windowProc(
+	HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
